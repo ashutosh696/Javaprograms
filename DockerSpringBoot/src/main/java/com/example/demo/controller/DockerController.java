3de936d47693f183ed68c9805b232a6ad33f146f
Ashutosh <ashutosh696@gmail.com>
2022-05-14 16:21:27 +0530

    Docker springboot

---
+++ DockerSpringBoot/src/main/java/com/example/demo/controller/DockerController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/docker")
+public class DockerController {
+
+	@GetMapping("/test")
+	@ResponseBody
+	public String getDoc()
+	{
+		return "hello Docker!!";
+	}
+}
